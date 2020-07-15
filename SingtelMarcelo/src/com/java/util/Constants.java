b9e41ad156d89ad749343b2b5c5fb552b7193f8a
Nicole Marie Marcelo <nicolemariemarcelo@gmai.com>
2020-07-15 21:50:16 +0800

    Can you add a second language (if you know a language other than English) Use the rooster as a PoC for demonstrating this. For example, this is how the Rooster sounds differently depending on language. Please add the rooster sound in your native tongue.

---
+++ SingtelMarcelo/src/com/java/util/Constants.java
@@ -0,0 +1,23 @@
+package com.java.util;
+
+public class Constants {
+
+	public static final String DANISH 	= "DANISH";
+	public static final String DUTCH 	= "DUTCH";
+	public static final String FILIPINO = "FILIPINO";
+	public static final String FINNISH 	= "FINNISH";
+	public static final String FRENCH 	= "FRENCH";
+	public static final String GERMAN 	= "GERMAN";
+	public static final String GREEK 	= "GREEK";
+	public static final String HEBREW 	= "HEBREW";
+	public static final String HUNGARIAN = "HUNGARIAN";
+	public static final String ITALIAN 	= "ITALIAN";
+	public static final String JAPANESE = "JAPANESE";
+	public static final String PORTUGUESE = "PORTUGUESE";
+	public static final String RUSSIAN 	= "RUSSIAN";
+	public static final String SWEDISH 	= "SWEDISH";
+	public static final String TURKISH 	= "TURKISH";
+	public static final String URDU 	= "URDU";
+	
+	
+}
