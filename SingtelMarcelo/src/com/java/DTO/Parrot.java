c9d938a416cba11fedc7c15ecd670f84a44ad783
Nicole Marie Marcelo <nicolemariemarcelo@gmai.com>
2020-07-15 21:26:08 +0800

    4. Can you model a parrot? We are specifically interested in three parrots, one that lived in a house with dogs one in a house with cats, the other lived on a farm next to the rooster. a. A parrot living with dogs says: “Woof, woof” b. A parrot living with cats says: “Meow” c. A parrot living near the rooster says: “Cock-a-doodle-doo” d. How do you keep the parrot maintainable? What if we need another parrot lives near a Duck? Or near a phone that rings frequently? --> Add more conditions if there are other cases needed in the future

---
+++ SingtelMarcelo/src/com/java/DTO/Parrot.java
@@ -0,0 +1,17 @@
+package com.java.DTO;
+
+public class Parrot extends Bird {
+
+	//Add more conditions for future enhancements
+	public void makeSounds(String livingWith) {
+		if(livingWith.equalsIgnoreCase("dogs")) {
+			System.out.println("Woof, woof");
+		} else if(livingWith.equalsIgnoreCase("cats")) {
+			System.out.println("Meow");
+		} else if(livingWith.equalsIgnoreCase("roosters")) {
+			Rooster rooster = new Rooster();
+			rooster.makeSounds();
+		}
+	}
+	
+}
