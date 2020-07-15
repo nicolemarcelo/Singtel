6387d57c253ce0b90e49ae50226c1b9f82905fff
Nicole Marie Marcelo <nicolemariemarcelo@gmai.com>
2020-07-15 21:14:08 +0800

    1. Can you implement the sing() method for the bird? a. How did you unit test it? --> By calling the function sing() in main
    b. How did you optimize the code for maintainability?
    --> Only add the function to a class you know will use it (so do not
    implement this on Animal class)

---
+++ SingtelMarcelo/src/com/java/DTO/Animal.java
@@ -0,0 +1,8 @@
+package com.java.DTO;
+
+public class Animal {
+	
+	public void walk(){
+		System.out.println("I am walking"); 
+	}
+}
---
+++ SingtelMarcelo/src/com/java/Main/Solution.java
@@ -0,0 +1,14 @@
+package com.java.Main;
+
+import com.java.DTO.Bird;
+
+public class Solution {
+
+	public static void main(String[] args) {
+		Bird bird = new Bird(); 
+		bird.walk(); 
+		bird.fly(); 
+		bird.sing();
+	}
+
+}
